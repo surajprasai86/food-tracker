dc91a381227e171dd35def6005d4a23d08e2174a
ujjwal khadka <u.khadka@jacobs-university.de>
2023-03-26 15:24:51 +0200

    food image added

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -12,6 +12,7 @@ function FoodCard({pageNumber}) {
   const data = useContext(DataContext).foodData.foodList
   // console.log("dta is ", data);
   const noOfCardsWantedInPage = 6
+
   return (
     <Container>
       <h1>Food List</h1>
@@ -20,9 +21,10 @@ function FoodCard({pageNumber}) {
           data.slice(noOfCardsWantedInPage*(pageNumber-1), noOfCardsWantedInPage*(pageNumber-1)+noOfCardsWantedInPage).map((food, index) => (
             <Col style={{marginBottom:"14px"}} sm={4} key={index}>
               <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="/pumpkin.jpg" />
+                <Card.Img variant="top" src={`/food/${String(food.name).toLowerCase()}.webp`} />
                 <Card.Body>
                   <Card.Title>{food.name}</Card.Title>
+                  {console.log(food.name)}
                   <Card.Text>
                     Some quick example text to build on the card title and make
                     up the bulk of the cards content.
